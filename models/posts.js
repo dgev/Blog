a46ec5d0b8931bde3411aaa5a43cbe6015dbdeb4
lilitotaryan <lilit_otaryan@edu.aua.am>
2019-05-18 19:16:24 +0400

    Update, delete of posts

--- models/posts.js
+++ models/posts.js
@@ -11,19 +11,17 @@ UserDoesNotHaveAPost
 
 async function createPost(email, title, description) {
   let user = await User.getUserByEmail(email);
-  console.log(user);
   let post = await Post.addPost(title, description);
-   user.posts.push(post._id);
-   //console.log(user.posts);
-  User.update({
-     email: email
-   },{
-     posts: user.posts
-   }, function(err, affected, resp) {
-     console.log(affected);
-   });
-   console.log(user.posts);
-  }
+  user.posts.push({_id: post._id});
+  await user.updateForDeleteCreate(user.posts);
+}
+
+async function deletePost(email, post) {
+      let user = await User.getUserByEmail(email);
+      user.posts.pull(post);
+      await user.updateForDeleteCreate(user.posts);
+      await Post.deleteThePost(post);
+}
 
 async function getPosts(){
   return await Post.getAllPosts();
@@ -43,42 +41,21 @@ async function getPostByID(id){
   return Post.getPostByID(id);
 }
 
+async function updatePostTitle(id, title) {
+  (await Post.getPostByID(id)).updateTitle(title);
+}
 
-  // async function updatePostTitle(post) {
-  //   let user = await User.getUserByEmail(post.email);
-  //    let post = user.posts.findOne({_id: post._id});
-  //    Post.updatePostTitle(post);
-  //    console.log(post);
-  //     User.update({
-  //      email: email
-  //    },{
-  //      posts: user.posts
-  //    }, function(err, affected, resp) {
-  //      console.log(affected);
-  //    });
-  //    console.log(user.posts);
-  //   }
-  //
-  // async function deletePost(email, post) {
-  //   let user = await getUser(email);
-  //   let index = user.posts.findIndex(p => p.split(':')[1].split(',')[0].trim() == post);
-  //     if(index<0){
-  //       throw new UserDoesNotHaveAPost();
-  //       //console.log(user);
-  //       //console.log(post);
-  //     }
-  //     console.log("I am into if statement");
-  //     Post.deletePost(post);
-  //   //  User.deleteUserPost(user, post, index);
-  //   // Call corresponding schema function to retrieve the user and return the result.
-  //   // if the user is not found, throw UserNotFound error.
-  // }
+async function updatePostDescription(id, description) {
+  (await Post.getPostByID(id)).updateDescription(description);
+}
+
 module.exports = {
   createPost,
   getPosts,
   getAllPostsOfTheUser,
   getRecentPosts,
   getPostByID
-  //deletePost,
-  //updatePostTitle
+  deletePost,
+  updatePostTitle,
+  updatePostDescription
 }
