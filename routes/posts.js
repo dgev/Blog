0443d650f0d78319c4d2302bdd7a39b5b20cafe0
lilitotaryan <lilit_otaryan@edu.aua.am>
2019-05-21 02:28:31 +0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -81,8 +81,7 @@ router.post('/deletePost', async function(req, res, next) {
 router.post('/updateTitleDesciption', async function(req, res, next) {
 
   try {
-    res.send(await updatePostTitle(req.body._id, req.body.title));
-    res.send(await updatePostDescription(req.body._id, req.body.description));
+    res.send(await updatePost(req.body._id, req.body.title, req.body.description));
     res.status(200).end();
   } catch (err) {
     next(err);
